894862e5bb09fe3ef59ba653f1f722a9745bcf46
julien <julien@papier-code.fr>
2017-04-13 15:52:02 +0200

    V0.4.2 wp.media sélection image condition

--- pc-tools-script.js
+++ pc-tools-script.js
@@ -3,8 +3,10 @@
 * [PC] Tools : fonctions javascript pour l'admin WP
 *
 * * Images upload
+* * Gallery Upload
 * * Pdf upload
 * * Date Picker
+* * Compteur
 *
 **/
 
@@ -71,18 +73,18 @@ if ( $('.pc-img-select').length > 0 ) {
 
                 // mise à jour du champ caché
                 $hiddenField.val(imgDatas.id);
+                var imgUrl = imgDatas.sizes.hasOwnProperty('thumbnail') ? imgDatas.sizes.thumbnail.url : imgDatas.url ;
 
                 // si une preview existe déjà
                 if ( $container.find('.pc-media-preview').length > 0 ) {
 
                     // modification de la preview
-                    $container.find('.pc-media-preview').html('<div class="pc-media-preview-item" style="background-image:url('+imgDatas.sizes.thumbnail.url+');"></div>');
+                    $container.find('.pc-media-preview').html('<div class="pc-media-preview-item" style="background-image:url('+imgUrl+');"></div>');
 
                 // si pas encore de preview
                 } else {
-
                     // ajoute la preview
-                	$container.prepend('<div class="pc-media-preview"><div class="pc-media-preview-item" style="background-image:url('+imgDatas.sizes.thumbnail.url+');"></div></div>');
+                    $container.prepend('<div class="pc-media-preview"><div class="pc-media-preview-item" style="background-image:url('+imgUrl+');"></div></div>');
                     // texte du bouton
                     $btnSelect.val('Modifier');
 
@@ -90,9 +92,9 @@ if ( $('.pc-img-select').length > 0 ) {
                     if ( $btnSelect.data('remove') == 'active') {
 
                         // ajoute le btn
- 	                	$btnSelect.after('<input class="button pc-media-remove" data-cible="image" type="button" value="Supprimer"/>');
+                        $btnSelect.after('<input class="button pc-media-remove" data-cible="image" type="button" value="Supprimer"/>');
                         // au clic sur le nouveau btn de suppression
-	                	$container.find('.pc-media-remove').click(function() { pc_media_remove( $(this) ); });
+                        $container.find('.pc-media-remove').click(function() { pc_media_remove( $(this) ); });
 
                     } // FIN if btn suppression
 
@@ -180,10 +182,12 @@ if ( $('.pc-gallery-select').length > 0 ) {
 
                 // mise à jour du champ caché
                 var imgIdsToSave = [],
-                    galleryPreview = '';
+                    galleryPreview = '',
+                    imgUrl;
                 for (var i = 0; i < galleryDatas.length; i++) {
                     imgIdsToSave.push(galleryDatas[i].id);
-                    galleryPreview += '<div class="pc-media-preview-item" style="background-image:url('+galleryDatas[i].sizes.thumbnail.url+');"></div>';
+                    imgUrl = galleryDatas[i].sizes.hasOwnProperty('thumbnail') ? galleryDatas[i].sizes.thumbnail.url : galleryDatas[i].url ;
+                    galleryPreview += '<div class="pc-media-preview-item" style="background-image:url('+imgUrl+');"></div>';
                 }
                 $hiddenField.val(imgIdsToSave.join());
 
@@ -316,21 +320,21 @@ if ( $('.pc-date-picker').length > 0 ) {
 
     $('.pc-date-picker').datepicker({
 
-    	closeText: "Fermer",
-    	prevText: "Précédent",
-    	nextText: "Suivant",
-    	currentText: "Aujourd'hui",
-    	monthNames: [ "janvier", "février", "mars", "avril", "mai", "juin", "juillet", "août", "septembre", "octobre", "novembre", "décembre" ],
-    	monthNamesShort: [ "janv.", "févr.", "mars", "avr.", "mai", "juin",	"juil.", "août", "sept.", "oct.", "nov.", "déc." ],
-    	dayNames: [ "dimanche", "lundi", "mardi", "mercredi", "jeudi", "vendredi", "samedi" ],
-    	dayNamesShort: [ "dim.", "lun.", "mar.", "mer.", "jeu.", "ven.", "sam." ],
-    	dayNamesMin: [ "D","L","M","M","J","V","S" ],
-    	weekHeader: "Sem.",
-    	dateFormat: "dd MM yy",
-    	firstDay: 1,
-    	isRTL: false,
-    	showMonthAfterYear: false,
-    	yearSuffix: ""
+        closeText: "Fermer",
+        prevText: "Précédent",
+        nextText: "Suivant",
+        currentText: "Aujourd'hui",
+        monthNames: [ "janvier", "février", "mars", "avril", "mai", "juin", "juillet", "août", "septembre", "octobre", "novembre", "décembre" ],
+        monthNamesShort: [ "janv.", "févr.", "mars", "avr.", "mai", "juin", "juil.", "août", "sept.", "oct.", "nov.", "déc." ],
+        dayNames: [ "dimanche", "lundi", "mardi", "mercredi", "jeudi", "vendredi", "samedi" ],
+        dayNamesShort: [ "dim.", "lun.", "mar.", "mer.", "jeu.", "ven.", "sam." ],
+        dayNamesMin: [ "D","L","M","M","J","V","S" ],
+        weekHeader: "Sem.",
+        dateFormat: "dd MM yy",
+        firstDay: 1,
+        isRTL: false,
+        showMonthAfterYear: false,
+        yearSuffix: ""
 
     });
 
@@ -380,5 +384,5 @@ if ( $('.pc-counter').length > 0 ) {
 
 
 /*=====  FIN Compteurs  ======*/
-	
+    
 }); // End document.ready
